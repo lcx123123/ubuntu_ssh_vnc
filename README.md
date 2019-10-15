3ad4bbcb5c19282c22828a9ea000f5baa6267ef2
lcx123123 <43430684+lcx123123@users.noreply.github.com>
2019-10-15 21:33:23 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ubuntu_ssh_vnc
-实训镜像文件，ssh+vnc的。
+# docker-ubuntu-ssh-vnc
+Ubuntu 18.04 with SSHD, VNC, XFCE, and chinese support.
+
+# how to run
+docker run -p 22:22 -p 5900:5900 wenkb/docker-ubuntu-ssh-vnc:v1.0
---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Start ssh server
+/usr/sbin/sshd -D &
+
+source /root/.bashrc
+
+# start vnc server
+vncserver -kill $VNC_DISPLAY
+rm -rfv /tmp/.X*-lock /tmp/.X11-unix
+vncserver $VNC_DISPLAY -geometry $VNC_GEOMETRY -securitytypes none
+tail -f /root/.vnc/*$VNC_DISPLAY.log
